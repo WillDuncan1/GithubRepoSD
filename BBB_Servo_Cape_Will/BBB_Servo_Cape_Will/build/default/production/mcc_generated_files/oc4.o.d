97ecd334824a9c9062e5db2296fc9e46496b2707
Sean <smketrin@ncsu.edu>
2016-02-11 22:10:21 -0500

    switching to new processor

--- BBB_Servo_Cape_Will/BBB_Servo_Cape_Will/build/default/production/mcc_generated_files/oc4.o.d
+++
@@ -1,3 +0,0 @@
-build/default/production/mcc_generated_files/oc4.o:  \
- mcc_generated_files/oc4.c  \
-mcc_generated_files/oc4.h 
--- BBB_Servo_Cape_Will/BBB_Servo_Cape_Will/build/default/production/mcc_generated_files/oc6.o.d
+++
@@ -1,3 +0,0 @@
-build/default/production/mcc_generated_files/oc6.o:  \
- mcc_generated_files/oc6.c  \
-mcc_generated_files/oc6.h 
